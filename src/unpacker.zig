31d0220994387c483f36e4add83ae5716cf39327
Francisco Freitas <contact@ffreitas.io>
2023-08-28 21:35:01 +0200

    refactor: moved to 0.11.0 std.coff API

--- src/unpacker.zig
+++ src/unpacker.zig
@@ -18,31 +18,22 @@ fn buildVirtualImage(binary: coff.Coff, allocator: mem.Allocator) ![]u8 {
     var imageSize: u32 = binary.getOptionalHeader64().size_of_image;
     var image: []u8 = try allocator.alloc(u8, imageSize);
     errdefer allocator.free(image);
-    var headers = binary.getSectionHeaders();
-    var dataStream = io.fixedBufferStream(data);
-    var dataReader = dataStream.reader();
     var imageStream = io.fixedBufferStream(image);
     var imageWriter = imageStream.writer();
     var imageHeader = data[0..binary.getOptionalHeader64().size_of_headers];
     _ = try imageWriter.write(imageHeader);
 
-    for (headers) |header| {
+    for (binary.getSectionHeaders()) |header| {
+        var section = binary.getSectionData(&header);
         var virtualPointer = header.virtual_address;
-        var rawPointer = header.pointer_to_raw_data;
-        var rawSize = header.size_of_raw_data;
-        try dataStream.seekTo(rawPointer);
         try imageStream.seekTo(virtualPointer);
-        var buffer = try allocator.alloc(u8, rawSize);
-        defer allocator.free(buffer);
-        _ = try dataReader.read(buffer);
-        _ = try imageWriter.write(buffer);
+        _ = try imageWriter.write(section);
     }
 
     return image;
 }
 
 pub fn unpack(binary: coff.Coff, allocator: mem.Allocator) !void {
-    const FALSE: c_int = 0;
     var image = try buildVirtualImage(binary, allocator);
     defer allocator.free(image);
 
@@ -50,10 +41,10 @@ pub fn unpack(binary: coff.Coff, allocator: mem.Allocator) !void {
     if (virtual == null) {
         return PackerError.BinaryMappingError;
     }
-    if (pe.ImageLoadImports(virtual) == FALSE) {
+    if (pe.ImageLoadImports(virtual) == pe.FALSE) {
         return PackerError.ImportLoadingError;
     }
-    if (pe.ImageRelocate(virtual) == FALSE) {
+    if (pe.ImageRelocate(virtual) == pe.FALSE) {
         return PackerError.RelocationError;
     }
     pe.ImageRunEntrypoint(virtual);
